1b5364f46f14da856fdcdd54bc7945f5254eae66
Hairunnisa <145535035+hairunnis@users.noreply.github.com>
2023-11-28 23:41:13 +0700

    config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php
 $dbhost = 'localhost';
-$dbuser = 'id21443593_dane';
-$dbpass = 'Zidaneotwitkonsultan1!';
-$dbname = 'id21443593_bukutamu';
+$dbuser = 'id21588515_indah';
+$dbpass = 'Nisa15012_';
+$dbname = 'id21588515_senja';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
 mysqli_select_db($conn, $dbname);
 ?>
